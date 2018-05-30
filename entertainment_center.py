29dabd4cee0d2b25480fd0d1adfa2abcb46209d0
Charles Stumpf <charles.stumpf@gmail.com>
2018-05-29 17:25:43 -0700

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,19 +1,14 @@
 import fresh_tomatoes
 import media
 
-toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life.",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-#print(toy_story.storyline)
-avatar = media.Movie("Avatar", "A marine on an alien planet.",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=uZNHIU3uHT4")
-#print(avatar.storyline)
-#avatar.show_trailer()
+north_by_northwest = media.Movie("North by Northwest", "A man fights a case of mistaken identity.",
+                                 "https://upload.wikimedia.org/wikipedia/commons/8/83/Northbynorthwest1.jpg"
+                                 "https://www.youtube.com/watch?v=qk0GbTMMbP0")
+
 fight_club = media.Movie("Fight Club", "Mischief, Mayhem, Soap.",
                          "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg",
                          "https://www.youtube.com/watch?v=SUXWAEX2jlg")
-#fight_club.show_trailer()
+
 oceans_11 = media.Movie("Ocean's 11", "11 con men take on a Las Vegas casino.",
                         "https://upload.wikimedia.org/wikipedia/en/6/68/Ocean%27s_Eleven_2001_Poster.jpg",
                         "https://www.youtube.com/watch?v=imm6OR605UI")
@@ -26,18 +21,9 @@ hail_caesar = media.Movie("Hail, Caesar!", "Cohen brothers comedy set in Hollywo
                           "https://upload.wikimedia.org/wikipedia/en/a/a9/Hail%2C_Caesar%21_poster.png",
                           "https://www.youtube.com/watch?v=kMqeoW3XRa0")
 
-movies = [toy_story, avatar, fight_club, oceans_11, the_prestige, hail_caesar]
-#fresh_tomatoes.open_movies_page(movies)
-#print(media.Movie.VALID_RATINGS)
-print(media.Movie.__doc__)
-print(media.Movie.__name__)
-print(media.Movie.__module__)
+jurassic_park = media.Movie("Jurassic Park", "An adventure 65 million years in the making",
+                            "https://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg",
+                            "https://www.youtube.com/watch?v=lc0UehYemQA")
 
-#north_by_northwest = media.Movie("North by Northwest", "A man fights a case of mistaken identity.",
-                                 #"https://upload.wikimedia.org/wikipedia/commons/8/83/Northbynorthwest1.jpg"
-                                 #"https://www.youtube.com/watch?v=qk0GbTMMbP0")
-
-#jurassic_park = media.Movie("Jurassic Park", "An adventure 65 million years in the making",
-                            #"https://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg",
-                            #"https://www.youtube.com/watch?v=lc0UehYemQA")
-
+movies = [north_by_northwest, fight_club, oceans_11, the_prestige, hail_caesar, jurassic_park]
+fresh_tomatoes.open_movies_page(movies)
